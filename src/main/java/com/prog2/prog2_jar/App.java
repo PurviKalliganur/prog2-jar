9bc476544a8b321af756366bf877b63efec914c0
PurviKalliganur <kpurvi05@gmail.com>
2026-06-21 17:23:59 +0530

    Initial Commit

---
+++ src/main/java/com/prog2/prog2_jar/App.java
@@ -0,0 +1,10 @@
+package com.prog2.prog2_jar;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/prog2/prog2_jar/AppTest.java
@@ -0,0 +1,12 @@
+package com.prog2.prog2_jar;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
